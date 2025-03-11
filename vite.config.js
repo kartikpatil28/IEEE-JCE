22eaea77d6194842c7a6e121112f4baab7ef5302
kartikpatil28 <kartikpatilwork24@gmail.com>
2025-03-11 09:06:22 +0530

    Setup git pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/IEEE-JCE/', // 🔹 Replace with your GitHub repository name (case-sensitive!)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
